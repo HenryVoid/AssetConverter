27f582413604c586d7d873a1db3a5f216b9a2eb7
woogi <azicplus@naver.com>
2025-02-26 21:26:26 +0900

    init project

---
+++ AssetConverter/AssetConverter/AssetConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetConverterApp.swift
+//  AssetConverter
+//
+//  Created by 송형욱 on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct AssetConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssetConverter/AssetConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AssetConverter
+//
+//  Created by 송형욱 on 2/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
